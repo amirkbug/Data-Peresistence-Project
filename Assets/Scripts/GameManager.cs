1ec57e750bbee616fb748f4d71e4738ee0a33ce5
amirkbug <amirworkplacett@gmail.com>
2024-10-29 11:50:11 +0330

    all done
    done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.IO;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     
     public static GameManager Instance;
+    public TMP_InputField usernameInputField;
+    public string username;
+    public TextMeshProUGUI title;
+
+    public bool textChange = false;
+    public bool textShowed = false;
+    public int hightScoreLoaded;
+
+
 
 
     private void Awake()
     {
+        
 
-
-        if(Instance != null)
+        if (Instance != null)
         {
             Destroy(gameObject);
             return;
@@ -25,20 +36,77 @@ public class GameManager : MonoBehaviour
 
         Instance = this; 
         DontDestroyOnLoad(gameObject);
+
+
+        
+        
+        //load data on awake and set the title to score and name
+        LoadData();
+        title.text =   username+ " : " + hightScoreLoaded; 
+        usernameInputField.text = username;
+
     }
+
 
 
     public void StartGame()
     {
+        
+        username = usernameInputField.text;
+
         SceneManager.LoadScene(1);
-    }
-
-
-    public void PlayerNameStore()
-    {
         
     }
 
 
-    
+    [System.Serializable]
+    class SaveData
+    {
+        public string username;
+        public int highScore;
+        
+    }
+
+    public void SaveAllData(int highScoreFromMain)
+    {
+        SaveData data = new SaveData();
+        data.username = username;
+        data.highScore = highScoreFromMain;
+        
+        string json = JsonUtility.ToJson(data);
+        Debug.Log(json);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+        
+    }
+
+    public void LoadData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            username = data.username;
+            hightScoreLoaded = data.highScore;
+            
+
+        }
+    }
+
+    public void OnChangeText()
+    {
+        if(usernameInputField.text != username)
+        {
+            textChange = true;
+            
+        }
+        else
+        {
+            textChange = false;
+            
+        }
+    }
+
+
+
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,21 +12,32 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
-    public GameObject GameOverText;
+    public Text nameAndScoretext;
     
+
+
+    public GameObject GameOverText;
+
     private bool m_Started = false;
     private int m_Points;
-    
-    private bool m_GameOver = false;
+
 
     
+
+    private bool m_GameOver = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        if (GameManager.Instance.textShowed)
+        {
+            nameAndScoretext.text = "Best Score : " + GameManager.Instance.username + " : " + GameManager.Instance.hightScoreLoaded;
+        }
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -36,10 +48,20 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
+        if(GameManager.Instance.textChange)
+        {
+            m_Points = 0;
+            GameManager.Instance.hightScoreLoaded = 0;
+        }
+
     }
 
     private void Update()
     {
+
+        
+
         if (!m_Started)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -57,7 +79,9 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
             }
         }
     }
@@ -66,11 +90,39 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+
     }
 
     public void GameOver()
     {
+        GameManager.Instance.SaveAllData(WichNumIsBigger());
+        GameManager.Instance.LoadData();
+        
         m_GameOver = true;
+        if (m_GameOver)
+        {
+            nameAndScoretext.text = "Best Score : " + GameManager.Instance.username + " : " + GameManager.Instance.hightScoreLoaded;
+            GameManager.Instance.textShowed = true;
+        }
         GameOverText.SetActive(true);
+        
+        
+
+
     }
+
+    private int WichNumIsBigger()
+    {
+        if(GameManager.Instance.hightScoreLoaded > m_Points)
+        {
+            return GameManager.Instance.hightScoreLoaded;
+        }
+        else
+        {
+            return m_Points;
+        }
+        
+        
+    }
+
 }
